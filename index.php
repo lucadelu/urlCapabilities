8e044d4bbab4fdd7422eb1f061a64fcd2178aa26
Luca Delucchi <lucadeluge@gmail.com>
2014-05-09 17:29:32 +0200

    add PHP variable for CSS file

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ include "php/language.php"
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
   <html xmlns="http://www.w3.org/1999/xhtml">
-  <link rel="stylesheet" type="text/css" href="css/gray.css">
+  <link rel="stylesheet" type="text/css" href="css/<?php echo $css; ?>">
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title><?php echo $title; ?></title>
@@ -31,7 +31,7 @@ include "php/language.php"
   </head>
   <body style="background-color:#FFFFFF">
       <div class="container" >
-          <h1 align="center"><?php echo$title; ?></h1>
+          <h1 align="center"><?php echo $title; ?></h1>
           <div class="left">
 <?php
 echo $lang["intro"];
--- urlcapabilities.php
+++ urlcapabilities.php
@@ -24,7 +24,7 @@ include "php/language.php"
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html xmlns="http://www.w3.org/1999/xhtml">
 <!--       <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet"> -->
-      <link rel="stylesheet" type="text/css" href="css/gray.css">
+      <link rel="stylesheet" type="text/css" href="css/<?php echo $css; ?>">
       <head>
           <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	  <title><?php echo $title; ?></title>
